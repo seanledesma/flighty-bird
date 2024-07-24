61bd38312c8efe2f7bbfb7066c2004d3669c3d1b
Sean Ledesma <seanledesma@protonmail.com>
2024-07-23 18:20:54 -0600

    cleanup flappy bird

--- playground/flappy_bird.c
+++ playground/flappy_bird.c
@@ -1,4 +1,4 @@
-    //basic window
+    // flappy bird w Raylib!
     #include "raylib.h"   
     
     #define screenWidth 1800
@@ -10,8 +10,6 @@
         Vector2 ballPos = { (float) screenWidth / 6, (float) screenHeight / 4 };
         Vector2 temp_pos = { ballPos.x, ballPos.y };
 
-        float spot = 0;
-
         bool falling = true;
 
         SetTargetFPS(60);
@@ -19,31 +17,9 @@
         //main game loop here
         while(!WindowShouldClose()) {
 
-            // if (IsKeyDown(KEY_D))
-            //     ballPos.x += 8.0f;
-            // if (IsKeyDown(KEY_LEFT))
-            //     ballPos.x -= 8.0f;
-            // if (IsKeyDown(KEY_UP))
-            //     ballPos.y -= 2.0f;  
-            // if (IsKeyDown(KEY_DOWN))
-            //     ballPos.y += 2.0f;  
-            
-
-            // if (IsKeyPressed(KEY_SPACE)){
-            //     falling = false;
-            //     if (ballPos.y == temp_pos.y)
-            //         temp_pos.y -= 60.0f;
-            //     else if (ballPos.y > temp_pos.y)
-            //         ballPos.y--;
-                
-            // }else if ()
-            //     ballPos.y++;
-            //     //ballPos.y -= 40.0f;
-
 
             if (IsKeyPressed(KEY_SPACE)) {
                 temp_pos = ballPos;
-                //spot = ballPos.y;
                 falling = false;
             }
 
@@ -54,17 +30,10 @@
                 else if (ballPos.y == temp_pos.y + 2)
                     falling = true;
                 else if (ballPos.y > temp_pos.y)
-                    ballPos.y -= 2;
-                // for (int i = 0; i < 40; i++) {
-                //     ballPos.y--;
-                // }
-                
+                    ballPos.y -= 2;                
             }else {
                 ballPos.y += 2;
             }
-
-
-
 
 
             BeginDrawing();
